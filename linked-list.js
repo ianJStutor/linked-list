8d5c7c24b61de2dc576dbf0af6bbdd48a18632fe
Ian Marshall <ian.js.tutor@gmail.com>
2022-09-03 21:46:27 -0400

    added find() method

--- linked-list.js
+++ linked-list.js
@@ -1,48 +1,59 @@
 class LinkedList {
 
-  constructor () {
-    this.head = this.tail = null;
-  }
+    constructor() {
+        this.head = this.tail = null;
+    }
 
-  addToTail (item) {
-    if (this.head === null){
-		this.tail = this.head = new ListNode(item);
-	}
-	else {
-		this.tail.next = new ListNode(item, this.tail);
-		this.tail = this.tail.next;
-	}
-    return this;
-  }
+    add(item) {
+        if (this.head === null){
+            this.tail = this.head = new ListNode(item);
+        }
+        else {
+            this.tail.next = new ListNode(item, this.tail);
+            this.tail = this.tail.next;
+        }
+        return this;
+    }
 
-  removeFromTail () {
-	if (!this.tail) return undefined;
-	var item = this.tail.item;
-	this.tail = this.tail.prev;
-	if (!this.tail){
-		this.head = this.tail = null;
-	}
-	else {
-		this.tail.next = null;
-	}
-	return item;
-  }
+    remove() {
+        if (!this.tail) return undefined;
+        var item = this.tail.item;
+        this.tail = this.tail.prev;
+        if (!this.tail){
+            this.head = this.tail = null;
+        }
+        else {
+            this.tail.next = null;
+        }
+        return item;
+    }
 
-  forEach (callbackFunc) {
-	var current = this.head;
-	while (current !== null) {
-		callbackFunc(current.item);
-		current = current.next;
-	}
-  }
+    find(callback) {
+        if ("function" !== typeof callback) return console.error("LinkedList.prototype.forEach requires a function as an argument.");
+        var current = this.head;
+        while (current !== null) {
+            if (callback(current.item)) return current.item;
+            current = current.next;
+        }
+        return undefined;
+    }
+
+    forEach(callback) {
+        if ("function" !== typeof callback) return console.error("LinkedList.prototype.forEach requires a function as an argument.");
+        var current = this.head;
+        while (current !== null) {
+            callback(current.item);
+            current = current.next;
+        }
+    }
 }
 
 class ListNode {
-  constructor (item, prev, next) {
-    this.item = item;
-    this.next = next || null;
-    this.prev = prev || null;
-  }
+constructor(item, prev, next) {
+this.item = item;
+this.next = next || null;
+this.prev = prev || null;
+}
 }
 
 export { LinkedList };
