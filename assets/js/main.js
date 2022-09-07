4c537cf319a7dcbc5c053dc0a7a3eac4af2a48bf
Ian Marshall <ian.js.tutor@gmail.com>
2022-09-07 14:39:37 -0400

    added speech synthesis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,28 @@
-import { betterRandom } from "../../better-random.js";
+import { LinkedList } from "../../linked-list.js";
 
-const userSeed = document.querySelector("fieldset:first-of-type input[type='number']");
+const ll = new LinkedList();
 
-document.querySelectorAll("input[type='radio']").forEach(r => {
-    r.addEventListener("change", () => {
-        if (userSeed.hasAttribute("disabled")) userSeed.removeAttribute("disabled");
-        else userSeed.setAttribute("disabled", "disabled");
-    });
-});
+const output = document.querySelector("output");
+const [removeButton, playButton] = document.querySelectorAll("main nav > button");
+removeButton.addEventListener("click", removeWord);
+playButton.addEventListener("click", playWords);
+document.querySelectorAll("main fieldset button").forEach(b => b.addEventListener("click", addWord));
 
-//Random Float
-document.querySelector("fieldset:nth-of-type(2) button").addEventListener("click", e => {
-    const rand = getRand(),
-          fieldset = e.target.closest("fieldset"),
-          output = fieldset.querySelector("output"),
-          qty = Number(fieldset.querySelector("input").value),
-          nums = [];
-    for (let i=0; i<qty; i++) nums.push(rand.nextFloat());
-    setOutput(output, nums);
-});
-
-//Random Integer within Range
-document.querySelector("fieldset:nth-of-type(3) button").addEventListener("click", e => {
-    const rand = getRand(),
-          fieldset = e.target.closest("fieldset"),
-          output = fieldset.querySelector("output"),
-          [qty, min, max] = Array.from(fieldset.querySelectorAll("input")).map(e => Number(e.value)),
-          rangedRand = rand.range(min, max),
-          nums = [];
-    for (let i=0; i<qty; i++) nums.push(rangedRand.nextInt());
-    output.innerHTML = nums.map(n => {
-        if (n === 1) n = "&#9856;";
-        else if (n === 2) n = "&#9857";
-        else if (n === 3) n = "&#9858";
-        else if (n === 4) n = "&#9859";
-        else if (n === 5) n = "&#9860";
-        else if (n === 6) n = "&#9861";
-        return `<span class="gameDie">${n}</span>`;
-    }).join("");
-});
-
-//Random Float within Range
-document.querySelector("fieldset:nth-of-type(4) button").addEventListener("click", e => {
-    const rand = getRand(),
-          fieldset = e.target.closest("fieldset"),
-          output = fieldset.querySelector("output"),
-          [qty, min, max] = Array.from(fieldset.querySelectorAll("input")).map(e => Number(e.value)),
-          rangedRand = rand.range(min, max),
-          nums = [];
-    for (let i=0; i<qty; i++) nums.push(rangedRand.nextFloat());
-    setOutput(output, nums);
-});
-
-function getRand() {
-    if (userSeed.hasAttribute("disabled")) return betterRandom();
-    return betterRandom(Number(userSeed.value));
+function addWord(e) {
+    ll.add(e.target.textContent);
+    updateOutput();
 }
 
-function setOutput(output, nums) {
-    output.innerHTML = `<ul>${
-        nums.map(n => `<li>${n}</li>`).join("")
-    }</ul>`;
+function removeWord() {
+    ll.remove();
+    updateOutput();
+}
+
+function playWords() {
+    const words = ll.toArray().join(" ");
+    speechSynthesis.speak(new SpeechSynthesisUtterance(words));
+}
+
+function updateOutput() {
+    output.textContent = ll.toArray().join(", ");
 }
